aab25907e41c911639b20412afe389b57f025e86
AntonioCastro <a22cagoan@iesgrancapitan.org>
2023-03-03 17:44:10 +0100

    issue cerrado y solucionado

--- java.java
+++ java.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
    public static void main(String[] args) {
-      system.out.println("¡Hola, mundo!");
+	// Muestra por pantalla el mensaje escrito
+      System.out.println("¡Hola, mundo!");
    }
 }
